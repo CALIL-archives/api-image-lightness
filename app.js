a1cfcc983c3bb49562b2f6769d73b572273e4f86
Satoshi Deguchi <deguchik@gmail.com>
2020-02-05 14:06:00 +0900

    update

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * HTTP Cloud Function.
  *
@@ -8,7 +7,10 @@
 
 'use strict';
 
-const { createCanvas, Image } = require('canvas')
+const {
+    createCanvas,
+    Image
+} = require('canvas')
 
 function AverageColorByImage(src) {
     const img = new Image();
@@ -16,50 +18,59 @@ function AverageColorByImage(src) {
     const canvas = createCanvas(200, 200)
     const ctx = canvas.getContext('2d');
     return new Promise((resolve, reject) => {
-      img.onload = () => {
-        canvas.height = img.height;
-        canvas.width = img.width;
-        ctx.drawImage(img, 0, 0);
-        const imageData = ctx.getImageData(0, 0, img.width, img.height)
-        let rgba = [0,0,0,0]     
-      
-        imageData.data.forEach((v,i) => {
-          rgba[i % 4] = rgba[i % 4] + v   
-        })
-        const r = rgba[0] / (img.width * img.height)
-        const g = rgba[1] / (img.width * img.height)
-        const b = rgba[2] / (img.width * img.height)
-        resolve([r,g,b])
-      };
-      img.onerror = (e) => reject(e)
+        img.onload = () => {
+            canvas.height = img.height;
+            canvas.width = img.width;
+            ctx.drawImage(img, 0, 0);
+            const imageData = ctx.getImageData(0, 0, img.width, img.height)
+            let rgba = [0, 0, 0, 0]
+
+            imageData.data.forEach((v, i) => {
+                rgba[i % 4] = rgba[i % 4] + v
+            })
+            const r = rgba[0] / (img.width * img.height)
+            const g = rgba[1] / (img.width * img.height)
+            const b = rgba[2] / (img.width * img.height)
+            resolve([r, g, b])
+        };
+        img.onerror = (e) => reject(e)
     });
 }
 
-function rgbToHsl(rgb){
-    const r = rgb[0] / 255, g = rgb[1] / 255, b = rgb[2] / 255;
-    const max = Math.max(r, g, b), min = Math.min(r, g, b);
+function rgbToHsl(rgb) {
+    const r = rgb[0] / 255,
+        g = rgb[1] / 255,
+        b = rgb[2] / 255;
+    const max = Math.max(r, g, b),
+        min = Math.min(r, g, b);
     let h, s, l = (max + min) / 2;
-  
-    if(max == min){
+
+    if (max == min) {
         h = s = 0; // achromatic
-    }else{
+    } else {
         var d = max - min;
         s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-        switch(max){
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
+        switch (max) {
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0);
+                break;
+            case g:
+                h = (b - r) / d + 2;
+                break;
+            case b:
+                h = (r - g) / d + 4;
+                break;
         }
         h /= 6;
     }
     return [h, s, l];
-  }
+}
 
 
 exports.imageLightness = (req, res) => {
 
     const isbns = req.get('isbns').split(',')
-    
+
     let hsls = []
     isbns.map(async (isbn, i) => {
         // 書影の平均色を出す
@@ -67,8 +78,11 @@ exports.imageLightness = (req, res) => {
         hsls.push(rgbToHsl(rgb))
         if (isbns.length === hsls.length) {
             res.type('application/json');
+            res.set('Access-Control-Allow-Methods', 'GET');
+            res.set('Access-Control-Allow-Headers', 'Content-Type');
+            res.set('Access-Control-Max-Age', '3600');
             res.status(200).send(hsls);
         }
-    })  
-   
+    })
+
 }
