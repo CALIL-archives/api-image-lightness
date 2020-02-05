bf2bcf09a46cb222859d0c504c5523fee22b562e
Satoshi Deguchi <deguchik@gmail.com>
2020-02-05 15:38:33 +0900

    update

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ exports.imageLightness = (req, res) => {
             res.set('Access-Control-Allow-Methods', 'GET');
             res.set('Access-Control-Allow-Headers', 'Content-Type');
             res.set('Access-Control-Max-Age', '3600');
+            res.set('cache-control', 'public, max-age=3600');
             res.status(200).send(hsls);
         }
     })
